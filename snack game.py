4aebf4d830c122329b0a0eeed44a6ddd7a3f9b99
186312313 <gholami.hannaaneh@gmail.com>
2025-01-29 23:15:33 +0330

    Update snake .py

--- snack game.py
+++ snack game.py
@@ -1,136 +1,206 @@
-#required libraries
 import turtle
 import time
 import random
-#game page
-wn = turtle.Screen()
-wn.title("snake game")
-wn.bgcolor("white")
-wn.setup(600, 600)
-wn.tracer(0)
-#speed control
+
+
+#زمان تأخير
 delay = 0.1
+
+#امتياز
 score = 0
 high_score = 0
-#snake head
+
+
+# تنظیم صفحه
+wn = turtle.Screen()
+wn.title("snake game")
+wn.bgcolor("grey")
+wn.setup(width=600, height=600)
+wn.tracer(0)  # خاموش کردن انیمیشن صفحه برای بهبود کارایی
+
+
+
+
+# ایجاد مار
 head = turtle.Turtle()
-colors = random.choice(['red', 'green', 'black', 'yellow', 'pink', 'brown', 'orange', 'purple'])
+head.speed(0)
 head.shape("square")
-head.color(colors)
+head.color("purple")
 head.penup()
-head.goto(0, 0)
+head.goto(0,0)  
 head.direction = "stop"
-#food
+
+#غذاي مار
 food = turtle.Turtle()
-colors = random.choice(['red', 'green', 'black', 'yellow', 'pink', 'orange', 'purple'])
-shaps = random.choice(['triangle', 'square', 'circle'])
-food.shape(shaps)
-food.color(colors)
+food.speed(0)
+food.shape("circle")
+food.color("white")
 food.penup()
-food.goto(0, 100)
-#score
+food.goto(0,100)
+
+segments = []
+
+#قلم
 pen = turtle.Turtle()
 pen.speed(0)
-pen.color("black")
+pen.shape("square")
+pen.color("green")
 pen.penup()
 pen.hideturtle()
-pen.goto(0, 250)
-pen.write("Score : 0 , high Score : 0",align = "center", font = ("candara", 24, "bold"))
-#move snake
+pen.goto(0,260)
+pen.write("Score:0 High score:0" , align = "center" , font=("Courier" , 24 , "normal"))
+
+
+#تابع حرکت مار
+
 def go_up():
-    if head.direction != "down" :
+    if head.direction != "down":
         head.direction = "up"
+
 def go_down():
-    if head.direction != "up" :
+    if head.direction != "up":
         head.direction = "down"
+
 def go_left():
-    if head.direction != "right" :
+    if head.direction != "right":
         head.direction = "left"
+
 def go_right():
-    if head.direction != "left" :
+    if head.direction != "left":
         head.direction = "right"
-#move snake head
-def move ():
-    if head.direction == "up" :
+
+
+    
+def move():
+    if head.direction == "up":
         y = head.ycor()
-        head.sety(y+20)
-    if head.direction == "down" :
+        head.sety(y + 20)
+
+    if head.direction == "down":
         y = head.ycor()
-        head.sety(y-20)
-    if head.direction == "left" :
-        x = head.xcor ()
-        head.setx(x-20)
-    if head.direction == "right" :
+        head.sety(y - 20)
+        
+    if head.direction == "left":
         x = head.xcor()
-        head.setx(x+20)
-#keys for control the snake
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+# کلیدها
 wn.listen()
-wn.onkeypress(go_up, "w")
-wn.onkeypress(go_down, "s")
-wn.onkeypress(go_left, "a")
-wn.onkeypress(go_right, "d")
-#snake body
-segments = []
-#ring for update the game
-while True :
+wn.onkeypress(go_up, " w ")    # حرکت به سمت بالا با کلید W
+wn.onkeypress(go_down, " s ")  # حرکت به سمت پایین با کلید S
+wn.onkeypress(go_left, " a ")   # حرکت به سمت چپ با کلید A
+wn.onkeypress(go_right, " d ")  # حرکت به سمت راست با کلید D
+
+        
+
+#حلقه اصلي بازي
+while True:
     wn.update()
-    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290 :
+
+    #بررسي برخورد با ديواره ها
+    if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
         time.sleep(1)
-        head.goto(0, 0)
+        head.goto(0,0)
         head.direction = "stop"
-        #reset the game
-        for segment in segments :
-            segment.goto(1000, 1000)
-        segment.clear()
+
+        #پنهان کردن
+        for segment in segments:
+            segment.goto(1000,1000)
+
+        #پاک کردن بخش هاي ليست
+        segments.clear()
+
+
+        #بررسي مجدد امتياز
         score = 0
-        pen.clear() 
-        pen.write("Score : {} High Score : {} ".format(score, high_score), align="center", font=("candara", 24, "bold"))
-        delay = 0.1
-    #snake body
-    if head.distance(food) < 20 :
+
+        #بررسي مجدد تأخير
+        delay=0.1
+
+        pen.clear()
+        pen.write("Score: {} High Score: {}".format(score , high_score), align="center" , font=("Courier" , 24 , "normal"))
+
+
+    #بررسي برخورد با غذا
+    if head.distance(food) < 20:
+        #حرکت تصادفي غذا
+        x = random.randint(-290 , 290)
+        y = random.randint(-290 , 290)
+        food.goto(x , y)
+
+        #اضافه کردن به بخش ها
         new_segment = turtle.Turtle()
         new_segment.speed(0)
         new_segment.shape("square")
-        colors = random.choice(['red', 'green', 'black', 'yellow', 'pink', 'brown', 'orange', 'purple'])
-        new_segment.color(colors)
+        new_segment.color("pink")
         new_segment.penup()
         segments.append(new_segment)
-        #new score
+
+
+        #کوتاه کردن تأخير
         delay -= 0.001
-        score += 10
-        if score > high_score :
+
+
+        #افزايش امتياز
+        score+=10
+
+        if score > high_score:
             high_score = score
+
         pen.clear()
-        pen.write("Score : {} High Score : {} ".format(score, high_score), align="center", font=("candara", 24, "bold"))
-        #move food
-        x = random.randint(-270, 270)
-        y = random.randint(-270, 270)
-        food.goto(x, y)
-    #move snake
-    for index in range (len(segments)-1, 0, -1):
-        #previous position of snake
+        pen.write("Score: {} High Score: {}".format(score , high_score), align="center" , font=("Courier" , 24 , "normal"))
+
+    #قسمت هاي انتهايي را به ترتيب معکوس حرکت مي دهيم
+    for index in range(len(segments)-1 , 0 , -1):
         x = segments[index-1].xcor()
         y = segments[index-1].ycor()
-        segments[index].goto(x, y)
-    if len(segments)>0:
+        segments[index].goto(x , y)
+        
+    #حرکت بخش 0 به جايي که سر است
+    if len(segments) >0:
         x = head.xcor()
         y = head.ycor()
-        segments[0].goto(x, y)
+        segments[0].goto(x , y)
+
     move()
-    #hit the snake head with snake body
-    for segment in segments:    
+
+    #بررسي برخورد سر با قسمت هاي بدن مار
+    for segment in segments:
         if segment.distance(head) < 20:
-            time.sleep(1) 
-            head.goto(0, 0) 
-            head.direction = "Stop"
-            #remove the snake body
-            for segment in segments:    
-                segment.goto(1000, 1000) 
-            segments.clear() 
-            score = 0  
+            time.sleep(1)
+            head.goto(0,0)
+            head.direction = "stop"
+            
+            #پنهان کردن
+            for segment in segments:
+                segment.goto(1000,1000)
+
+            #پاک کردن بخش هاي ليست
+            segments.clear()
+
+
+
+            #بررسي مجدد امتياز
+            score = 0
+
+
+            #بررسي مجدد تأخير
+            delay=0.1
+
+            #بروزرساني نمايش امتياز
             pen.clear()
-            pen.write("Score : {} High Score : {} ".format(score, high_score), align="center", font=("candara", 24, "bold"))
-            delay = 0.1   
+            pen.write("Score: {} High Score: {}".format(score , high_score), align="center" , font=("Courier" , 24 , "normal"))
+
+
+            
     time.sleep(delay)
+
+
+
+
 wn.mainloop()
-    
+
